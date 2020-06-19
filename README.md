fd4c2734598e9c2b8049e00b7d412f0cb4f2a788
Frederik Shull <frederik.shull@protonmail.com>
2020-06-19 13:36:24 +0200

    readme fix + bot formatting

--- README.md
+++ README.md
@@ -29,7 +29,11 @@ Connect a Garry's Mod Server with a Discord Server!
 
 ### What It Does
 
-When you die in Garry's Mod Trouble in Terrorist Town, you normally are not allowed to speak anymore. You will be muted automatically in game and can't talk anymore until you respawn. Since some people use Discord as an alternative voice chat with higher quality the in game voice chat is less in use. In Discord you have to mute yourself or be silent "manually" to avoid disturbance. TTTDiscordZL is a setup that connects your Garry's Mod server and your Discord server to automatically mute players who die and unmute them when they get revived or respawn.
+When you die in Garry's Mod Trouble in Terrorist Town, you normally are not allowed to speak anymore. You will be muted
+automatically in game and can't talk anymore until you respawn. Since some people use Discord as an alternative voice
+chat with higher quality the in game voice chat is less in use. In Discord you have to mute yourself or be silent
+"manually" to avoid disturbance. TTTDiscordZL is a setup that connects your Garry's Mod server and your Discord server
+to automatically mute players who die and unmute them when they get revived or respawn.
 
 ### How It Works
 
@@ -41,7 +45,8 @@ Basically there are three components that work together to make the magic happen
 
 And this is how they work together:
 
-1. When someone dies or respawns in game the server script will send a request to my (or your) webpage including the info which player to target and whether to mute or unmute them.
+1. When someone dies or respawns in game the server script will send a request to my (or your) webpage including the
+   info which player to target and whether to mute or unmute them.
 2. The webpage will edit the specific table corresponding to your Discord server which was created by the Discord bot.
 3. The Discord bot will periodically check if a player has to be muted or unmuted by observing the database.
 
@@ -79,25 +84,30 @@ Here are a couple of additional documentations that may help you.
 3. Insert your token you received from the Discord bot into the quotation marks at `local Token = ""`.
 4. Restart your Garry's Mod server.
 5. [**OPTIONAL**] Only if you want to use your own Discord server & database:
-   - Change the URL in the quotation marks to your own URL pointing to TTTDiscordZL.php at `local Webpage = "https://old.zlyfer.net/sites/games/gmod_ttt/TTTDiscordZL.php"`.
+   - Change the URL in the quotation marks to your own URL pointing to TTTDiscordZL.php at
+     `local Webpage = "https://old.zlyfer.net/sites/games/gmod_ttt/TTTDiscordZL.php"`.
 
 ### Add The Discord Bot To Your Own Discord Server
 
-1. Use this link to add my bot TTTDiscordZL to your Discord server: [Discord Bot Invite](https://discordapp.com/oauth2/authorize?client_id=424687518966087682&scope=bot&permissions=4197376).
+1. Use this link to add my bot TTTDiscordZL to your Discord server:
+   [Discord Bot Invite](https://discordapp.com/oauth2/authorize?client_id=424687518966087682&scope=bot&permissions=4197376).
 
 - Keep in mind that the bot needs following rights: _MUTE_MEMBERS_ and _VIEW_CHANNEL_.
 - The permission for _SEND_MESSAGES_ is not mandatory but **extremely recommended**!
 - See [Discord Bot Permissions](https://discordapp.com/developers/docs/topics/permissions) for reference.
 
-2. Upon invitation the bot will send you a token. As [mentioned](#install-garrys-mod-server-script), you need to insert this token inside of _TTTDiscordZL.lua_.
+2. Upon invitation the bot will send you a token. As [mentioned](#install-garrys-mod-server-script), you need to insert
+   this token inside of _TTTDiscordZL.lua_.
 
 ### Configurate The Discord Bot
 
 Every player has to tell the bot which SteamID64 they have.
 
-Basically you have to use the command `~zltd~link [SteamID64]` to link your DiscordID with your SteamID64 so the bot can mute you when you die.
+Basically you have to use the command `~zltd~link [SteamID64]` to link your DiscordID with your SteamID64 so the bot can
+mute you when you die.
 
-I made an [in depth tutorial](idlink.md) which should help on how to access your SteamID64 and how to properly link it with your DiscordID.
+I made an [in depth tutorial](idlink.md) which should help on how to access your SteamID64 and how to properly link it
+with your DiscordID.
 
 ### (OPTIONAL) Setup Own Discord Bot
 
@@ -107,21 +117,18 @@ I made an [in depth tutorial](idlink.md) which should help on how to access your
 
 #### Using [npm](https://www.npmjs.com/)
 
-    ```
     npm install
     npm start
-    ```
 
 #### Using [yarn](https://yarnpkg.com/)
 
-    ```
     yarn install
     yarn start
-    ```
 
 ### (OPTIONAL) Setup Own Database And Webpage
 
-1. Move _TTTDiscordZL.php_ to a directory on your webserver. Make sure it is exactly where `local Webpage` in _TTTDiscordZL.lua_ is pointing to.
+1. Move _TTTDiscordZL.php_ to a directory on your webserver. Make sure it is exactly where `local Webpage` in
+   _TTTDiscordZL.lua_ is pointing to.
 2. Fill in your database credentials using the variables in _TTTDiscordZL.php_.
 3. Create a database called 'TTTDiscordZL' and invite your Discord bot to your Discord server.
 
@@ -129,32 +136,44 @@ I made an [in depth tutorial](idlink.md) which should help on how to access your
 
 ## How To Basic
 
-If you don't know how to make your own Discord bot, install npm or similar you should stick to the [Simple Way](#1-simple-way).
+If you don't know how to make your own Discord bot, install npm or similar you should stick to the
+[Simple Way](#1-simple-way).
 
-In case you want to use the [Advanced Way](#2-advanced-way) anyway the links below might help you but please read the [Disclaimer](#disclaimer) first.
+In case you want to use the [Advanced Way](#2-advanced-way) anyway the links below might help you but please read the
+[Disclaimer](#disclaimer) first.
 
-- How to [make](https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/) a Discord bot and invite it to your Discord server.
-- What is [npm](https://docs.npmjs.com/getting-started/what-is-npm) or [yarn](https://yarnpkg.com/en/docs/getting-started).
+- How to [make](https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/) a Discord bot and invite it to your
+  Discord server.
+- What is [npm](https://docs.npmjs.com/getting-started/what-is-npm) or
+  [yarn](https://yarnpkg.com/en/docs/getting-started).
 - Yarn [vs](https://blog.risingstack.com/yarn-vs-npm-node-js-package-managers/) npm.
-- How to install [npm](https://www.npmjs.com/get-npm) or [yarn](https://yarnpkg.com/en/docs/install#windows-stable) on Windows.
-- How to install [npm](https://blog.teamtreehouse.com/install-node-js-npm-linux) or [yarn](https://yarnpkg.com/lang/en/docs/install/#debian-stable) on Linux.
-- How to open the [Command Prompt](https://www.lifewire.com/how-to-open-command-prompt-2618089) or [PowerShell](https://www.tenforums.com/tutorials/25581-open-windows-powershell-windows-10-a.html) on Windows.
-- How to open the [Terminal](https://www.lifewire.com/ways-to-open-a-terminal-console-window-using-ubuntu-4075024) on Linux.
+- How to install [npm](https://www.npmjs.com/get-npm) or [yarn](https://yarnpkg.com/en/docs/install#windows-stable) on
+  Windows.
+- How to install [npm](https://blog.teamtreehouse.com/install-node-js-npm-linux) or
+  [yarn](https://yarnpkg.com/lang/en/docs/install/#debian-stable) on Linux.
+- How to open the [Command Prompt](https://www.lifewire.com/how-to-open-command-prompt-2618089) or
+  [PowerShell](https://www.tenforums.com/tutorials/25581-open-windows-powershell-windows-10-a.html) on Windows.
+- How to open the [Terminal](https://www.lifewire.com/ways-to-open-a-terminal-console-window-using-ubuntu-4075024) on
+  Linux.
 
 ## FAQ
 
 ### Q: How do I get my token?
 
-- In case you didn't receive a token when you invited the Discord bot to your Discord server you can use the command `~zltd~token` to generate a new token.
+- In case you didn't receive a token when you invited the Discord bot to your Discord server you can use the command
+  `~zltd~token` to generate a new token.
 
 ## Disclaimer
 
 ### About The Advanced Way
 
-The links and tutorials are quickly searched in a haste. I am not responsible if you mess anything up but you can always open an issue if you have questions. In case you are unsure or worried, please use the [Simple Way](#1-simple-way) as mentioned.
+The links and tutorials are quickly searched in a haste. I am not responsible if you mess anything up but you can always
+open an issue if you have questions. In case you are unsure or worried, please use the [Simple Way](#1-simple-way) as
+mentioned.
 
 ### General Use Of TTTDiscordZL
 
 The bot will be able to server-side un/mute people on your discord server!
 
-~~At the moment everyone can mute people on your Discord server if they know it's ID and the SteamID64 of a specific person on your Discord server (if linked in database).~~ - **Tokens are now a thing!**
+~~At the moment everyone can mute people on your Discord server if they know it's ID and the SteamID64 of a specific
+person on your Discord server (if linked in database).~~ - **Tokens are now a thing!**
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// jshint esversion: 9
 process.chdir("/home/zlyfer/DiscordBots/TTTDiscordZL");
 const mysql = require("mysql");
 const Discord = require("discord.js");
@@ -6,15 +7,15 @@ const uniqid = require("uniqid");
 const token = require("./token.json");
 const mysql_config = require("./mysql_config.json");
 var sql = mysql.createConnection({
-	host: mysql_config.host,
-	user: mysql_config.user,
-	password: mysql_config.password,
-	database: mysql_config.database
+  host: mysql_config.host,
+  user: mysql_config.user,
+  password: mysql_config.password,
+  database: mysql_config.database,
 });
 
-sql.connect(function(err) {
-	if (err) throw err;
-	console.log("Connected to MySQL Database!");
+sql.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to MySQL Database!");
 });
 
 const client = new Discord.Client();
@@ -22,86 +23,90 @@ client.login(token.token);
 const botPrefix = "~zltd~";
 
 function checkPerm(guild, permission) {
-	const botID = client.user.id;
-	let hasPerm = guild.members.find("id", botID).hasPermission(permission);
-	return hasPerm;
+  const botID = client.user.id;
+  let hasPerm = guild.members.find("id", botID).hasPermission(permission);
+  return hasPerm;
 }
 
 function getMember(guild, DiscordID) {
-	let member = guild.members.find("id", DiscordID);
-	return member;
+  let member = guild.members.find("id", DiscordID);
+  return member;
 }
 
 function getDiscordID(GuildID, SteamID64, callback) {
-	sql.query("SELECT DiscordID FROM `" + GuildID + "` WHERE SteamID64 = " + SteamID64, (err, result) => {
-		if (err) throw err;
-		if (result) {
-			var DiscordID = false;
-			for (let i = 0; i < result.length; i++) {
-				if ("DiscordID" in result[i]) {
-					var DiscordID = result[i].DiscordID;
-				}
-				callback(DiscordID);
-			}
-		}
-	});
+  sql.query("SELECT DiscordID FROM `" + GuildID + "` WHERE SteamID64 = " + SteamID64, (err, result) => {
+    if (err) throw err;
+    if (result) {
+      var DiscordID = false;
+      for (let i = 0; i < result.length; i++) {
+        if ("DiscordID" in result[i]) {
+          var DiscordID = result[i].DiscordID;
+        }
+        callback(DiscordID);
+      }
+    }
+  });
 }
 
 function getSteamID64(GuildID, DiscordID, callback) {
-	sql.query("SELECT SteamID64 FROM `" + GuildID + "` WHERE DiscordID = " + DiscordID, (err, result) => {
-		if (err) throw err;
-		if (result) {
-			var SteamID64 = false;
-			for (let i = 0; i < result.length; i++) {
-				if ("SteamID64" in result[i]) {
-					var SteamID64 = result[i].SteamID64;
-				}
-				callback(SteamID64);
-			}
-		}
-	});
+  sql.query("SELECT SteamID64 FROM `" + GuildID + "` WHERE DiscordID = " + DiscordID, (err, result) => {
+    if (err) throw err;
+    if (result) {
+      var SteamID64 = false;
+      for (let i = 0; i < result.length; i++) {
+        if ("SteamID64" in result[i]) {
+          var SteamID64 = result[i].SteamID64;
+        }
+        callback(SteamID64);
+      }
+    }
+  });
 }
 
 function checkStatus(guild) {
-	if (checkPerm(guild, "MUTE_MEMBERS")) {
-		let GuildID = guild.id;
-		sql.query("SELECT * FROM `" + GuildID + "`", (err, result) => {
-			if (result) {
-				if (result.length != 0) {
-					for (let i = 1; i < result.length; i++) {
-						let resulti = result[i];
-						let member = getMember(guild, resulti.DiscordID);
-						if (member) {
-							if (resulti.Muted == "1" && member.serverMute == false) {
-								member.setMute(true).then(console.log(`Muted ${member.user.id} from ${GuildID}. Reason: Set to muted.`));
-							} else if (resulti.Muted == "0" && member.serverMute == true) {
-								member.setMute(false).then(console.log(`Unmuted ${member.user.id} from ${GuildID}. Reason: Set to unmuted.`));
-							}
-						}
-					}
-				}
-			}
-		});
-	}
+  if (checkPerm(guild, "MUTE_MEMBERS")) {
+    let GuildID = guild.id;
+    sql.query("SELECT * FROM `" + GuildID + "`", (err, result) => {
+      if (result) {
+        if (result.length != 0) {
+          for (let i = 1; i < result.length; i++) {
+            let resulti = result[i];
+            let member = getMember(guild, resulti.DiscordID);
+            if (member) {
+              if (resulti.Muted == "1" && member.serverMute == false) {
+                member
+                  .setMute(true)
+                  .then(console.log(`Muted ${member.user.id} from ${GuildID}. Reason: Set to muted.`));
+              } else if (resulti.Muted == "0" && member.serverMute == true) {
+                member
+                  .setMute(false)
+                  .then(console.log(`Unmuted ${member.user.id} from ${GuildID}. Reason: Set to unmuted.`));
+              }
+            }
+          }
+        }
+      }
+    });
+  }
 }
 
 function linkIDs(GuildID, DiscordID, SteamID64) {
-	sql.query(
-		"INSERT INTO `" +
-			GuildID +
-			"` (DiscordID, SteamID64, Muted) VALUES (" +
-			DiscordID +
-			", " +
-			SteamID64 +
-			", '0') ON DUPLICATE KEY UPDATE DiscordID = " +
-			DiscordID +
-			", SteamID64 = " +
-			SteamID64 +
-			", Muted = 0",
-		(err, result) => {
-			if (err) throw err;
-		}
-	);
+  sql.query(
+    "INSERT INTO `" +
+      GuildID +
+      "` (DiscordID, SteamID64, Muted) VALUES (" +
+      DiscordID +
+      ", " +
+      SteamID64 +
+      ", '0') ON DUPLICATE KEY UPDATE DiscordID = " +
+      DiscordID +
+      ", SteamID64 = " +
+      SteamID64 +
+      ", Muted = 0",
+    (err, result) => {
+      if (err) throw err;
+    }
+  );
 }
 
 // Not used yet.
@@ -112,205 +117,211 @@ function linkIDs(GuildID, DiscordID, SteamID64) {
 // }
 
 function unmute(GuildID, SteamID64) {
-	sql.query("UPDATE `" + GuildID + "` SET Muted = '0' WHERE SteamID64 = " + SteamID64, (err, result) => {
-		if (err) throw err;
-	});
+  sql.query("UPDATE `" + GuildID + "` SET Muted = '0' WHERE SteamID64 = " + SteamID64, (err, result) => {
+    if (err) throw err;
+  });
 }
 
 function init(guild) {
-	sql.query(
-		"CREATE TABLE IF NOT EXISTS `tokens` (GuildID VARCHAR(64) NOT NULL, Token VARCHAR(18) NOT NULL, TokenSent TINYINT(1) NOT NULL, UNIQUE ID (GuildID))",
-		(err, result) => {
-			if (err) throw err;
-		}
-	);
-	guildInit(guild);
-	checkToken(guild);
+  sql.query(
+    "CREATE TABLE IF NOT EXISTS `tokens` (GuildID VARCHAR(64) NOT NULL, Token VARCHAR(18) NOT NULL, TokenSent TINYINT(1) NOT NULL, UNIQUE ID (GuildID))",
+    (err, result) => {
+      if (err) throw err;
+    }
+  );
+  guildInit(guild);
+  checkToken(guild);
 }
 
 function tokenProcess(tokenCheck, guild) {
-	switch (tokenCheck) {
-		case "create":
-			createToken(guild);
-			break;
-		case "send":
-			sendToken(guild);
-			break;
-		case false:
-			break;
-	}
+  switch (tokenCheck) {
+    case "create":
+      createToken(guild);
+      break;
+    case "send":
+      sendToken(guild);
+      break;
+    case false:
+      break;
+  }
 }
 
 function guildInit(guild) {
-	sql.query(
-		"CREATE TABLE IF NOT EXISTS `" +
-			guild.id +
-			"` (DiscordID VARCHAR(64) NOT NULL, SteamID64 VARCHAR(64) NOT NULL, Muted TINYINT(1) NOT NULL, UNIQUE ID (DiscordID))",
-		(err, result) => {
-			if (err) throw err;
-		}
-	);
+  sql.query(
+    "CREATE TABLE IF NOT EXISTS `" +
+      guild.id +
+      "` (DiscordID VARCHAR(64) NOT NULL, SteamID64 VARCHAR(64) NOT NULL, Muted TINYINT(1) NOT NULL, UNIQUE ID (DiscordID))",
+    (err, result) => {
+      if (err) throw err;
+    }
+  );
 }
 
 function checkToken(guild) {
-	sql.query("SELECT TokenSent FROM `tokens` WHERE GuildID = " + guild.id, (err, result) => {
-		if (err) throw err;
-		if (result) {
-			if (result.length != 0) {
-				let resulti = result[0];
-				if (resulti.TokenSent == 1) {
-					tokenProcess(false, guild);
-				} else if (resulti.TokenSent == 0) {
-					tokenProcess("send", guild);
-				}
-			} else {
-				tokenProcess("create", guild);
-			}
-		}
-	});
+  sql.query("SELECT TokenSent FROM `tokens` WHERE GuildID = " + guild.id, (err, result) => {
+    if (err) throw err;
+    if (result) {
+      if (result.length != 0) {
+        let resulti = result[0];
+        if (resulti.TokenSent == 1) {
+          tokenProcess(false, guild);
+        } else if (resulti.TokenSent == 0) {
+          tokenProcess("send", guild);
+        }
+      } else {
+        tokenProcess("create", guild);
+      }
+    }
+  });
 }
 
 function createToken(guild) {
-	let token = uniqid();
-	sql.query(
-		"INSERT INTO `tokens` (GuildID, Token, TokenSent) VALUES (" + guild.id + ", '" + token + "', 0) ON DUPLICATE KEY UPDATE Token = '" + token + "'",
-		(err, result) => {
-			if (err) throw err;
-		}
-	);
-	sendToken(guild);
+  let token = uniqid();
+  sql.query(
+    "INSERT INTO `tokens` (GuildID, Token, TokenSent) VALUES (" +
+      guild.id +
+      ", '" +
+      token +
+      "', 0) ON DUPLICATE KEY UPDATE Token = '" +
+      token +
+      "'",
+    (err, result) => {
+      if (err) throw err;
+    }
+  );
+  sendToken(guild);
 }
 
 function sendToken(guild) {
-	sql.query("SELECT Token FROM `tokens` WHERE GuildID = " + guild.id, (err, result) => {
-		if (err) throw err;
-		if (result.length != 0) {
-			let resulti = result[0];
-			guild.owner
-				.send(
-					"This is your token: `" +
-						resulti.Token +
-						"`.\nYou need to insert this token inside the _TTTDiscordZL.lua_ file.\nFor more information visit https://github.com/zlyfer/TTTDiscordZL#install-garrys-mod-server-script.\nHave fun!"
-				)
-				.then(() => {
-					sql.query("UPDATE `tokens` SET TokenSent = '1' WHERE GuildID = " + guild.id, (err, result) => {
-						if (err) throw err;
-					});
-				});
-		}
-	});
+  sql.query("SELECT Token FROM `tokens` WHERE GuildID = " + guild.id, (err, result) => {
+    if (err) throw err;
+    if (result.length != 0) {
+      let resulti = result[0];
+      guild.owner
+        .send(
+          "This is your token: `" +
+            resulti.Token +
+            "`.\nYou need to insert this token inside the _TTTDiscordZL.lua_ file.\nFor more information visit https://github.com/zlyfer/TTTDiscordZL#install-garrys-mod-server-script.\nHave fun!"
+        )
+        .then(() => {
+          sql.query("UPDATE `tokens` SET TokenSent = '1' WHERE GuildID = " + guild.id, (err, result) => {
+            if (err) throw err;
+          });
+        });
+    }
+  });
 }
 
-client.on("message", message => {
-	var Guild = message.guild;
-	var Author = message.author;
-	var Channel = message.channel;
-	var MessageContent = message.content;
-	if (MessageContent.indexOf(botPrefix) != -1) {
-		if (!Author.bot) {
-			MessageContent = MessageContent.replace(botPrefix, "");
-			MessageContent = String(MessageContent).split(" ");
-			var cmd = MessageContent[0];
-			var msg = MessageContent[1];
-			if (Channel.type == "text") {
-				var GuildID = Guild.id;
-				var AuthorID = Author.id;
-				var Member = getMember(Guild, AuthorID);
-				switch (cmd) {
-					case "help":
-						var helpObj = {
-							help: {
-								parameter: "none",
-								desc: "Shows this help message."
-							},
-							link: {
-								parameter: "SteamID64",
-								desc: "Links your SteamID64 with your DiscordID."
-							},
-							unmute: {
-								parameter: "none",
-								desc: "Unmutes you - even if you are already dead."
-							},
-							token: {
-								parameter: "none",
-								desc: "Generate a new token. Only usable by the Discord server owner!"
-							}
-						};
-						var reply = `help is on the way:\n`;
-						reply += "Make sure to use **" + botPrefix + "** as prefix!\n";
-						reply += "The format is: **COMMAND** __PARAMETER__ - *DESCRIPTION*.\n\n";
-						for (var key in helpObj) {
-							reply += "**" + key + "** __" + helpObj[key].parameter + "__ - *" + helpObj[key].desc + "*\n";
-						}
-						reply += "\nINFO: If you encounter any issues or have questions, feel free to contact me.\n";
-						message.reply(reply);
-						break;
-					case "link":
-						if (msg) {
-							var SteamID64 = msg;
-							linkIDs(GuildID, AuthorID, SteamID64);
-							// message.reply(`I linked your DiscordID **${AuthorID}** with SteamID64 **${SteamID64}**.`);
-							message.react("👍");
-							setTimeout(
-								function() {
-									message.delete();
-								},
-								3000,
-								message
-							);
-						} else {
-							message.reply(`please provide your SteamID64.`);
-							message.delete();
-						}
-						break;
-					case "unmute":
-						getSteamID64(GuildID, AuthorID, function(SteamID64) {
-							if (SteamID64) {
-								unmute(GuildID, SteamID64);
-							}
-						});
-						message.channel.send(`${Member} unmuted him-/herself.`);
-						break;
-					case "token":
-						if (message.author.id == message.guild.owner.id) {
-							createToken(message.guild);
-						} else {
-							message.author.send(`Sorry, only the Discord server owner can request a new token.`);
-						}
-						break;
-					default:
-						message.reply(`sorry I didn't understand that. Use **${botPrefix}help** to see all available commands.`);
-						break;
-				}
-			} else {
-				message.reply(`Sorry I am supposed to be controlled via a text channel on a discord server.`);
-			}
-		}
-	}
+client.on("message", (message) => {
+  var Guild = message.guild;
+  var Author = message.author;
+  var Channel = message.channel;
+  var MessageContent = message.content;
+  if (MessageContent.indexOf(botPrefix) != -1) {
+    if (!Author.bot) {
+      MessageContent = MessageContent.replace(botPrefix, "");
+      MessageContent = String(MessageContent).split(" ");
+      var cmd = MessageContent[0];
+      var msg = MessageContent[1];
+      if (Channel.type == "text") {
+        var GuildID = Guild.id;
+        var AuthorID = Author.id;
+        var Member = getMember(Guild, AuthorID);
+        switch (cmd) {
+          case "help":
+            var helpObj = {
+              help: {
+                parameter: "none",
+                desc: "Shows this help message.",
+              },
+              link: {
+                parameter: "SteamID64",
+                desc: "Links your SteamID64 with your DiscordID.",
+              },
+              unmute: {
+                parameter: "none",
+                desc: "Unmutes you - even if you are already dead.",
+              },
+              token: {
+                parameter: "none",
+                desc: "Generate a new token. Only usable by the Discord server owner!",
+              },
+            };
+            var reply = `help is on the way:\n`;
+            reply += "Make sure to use **" + botPrefix + "** as prefix!\n";
+            reply += "The format is: **COMMAND** __PARAMETER__ - *DESCRIPTION*.\n\n";
+            for (var key in helpObj) {
+              reply += "**" + key + "** __" + helpObj[key].parameter + "__ - *" + helpObj[key].desc + "*\n";
+            }
+            reply += "\nINFO: If you encounter any issues or have questions, feel free to contact me.\n";
+            message.reply(reply);
+            break;
+          case "link":
+            if (msg) {
+              var SteamID64 = msg;
+              linkIDs(GuildID, AuthorID, SteamID64);
+              // message.reply(`I linked your DiscordID **${AuthorID}** with SteamID64 **${SteamID64}**.`);
+              message.react("👍");
+              setTimeout(
+                function () {
+                  message.delete();
+                },
+                3000,
+                message
+              );
+            } else {
+              message.reply(`please provide your SteamID64.`);
+              message.delete();
+            }
+            break;
+          case "unmute":
+            getSteamID64(GuildID, AuthorID, function (SteamID64) {
+              if (SteamID64) {
+                unmute(GuildID, SteamID64);
+              }
+            });
+            message.channel.send(`${Member} unmuted him-/herself.`);
+            break;
+          case "token":
+            if (message.author.id == message.guild.owner.id) {
+              createToken(message.guild);
+            } else {
+              message.author.send(`Sorry, only the Discord server owner can request a new token.`);
+            }
+            break;
+          default:
+            message.reply(`sorry I didn't understand that. Use **${botPrefix}help** to see all available commands.`);
+            break;
+        }
+      } else {
+        message.reply(`Sorry I am supposed to be controlled via a text channel on a discord server.`);
+      }
+    }
+  }
 });
 
-client.on("guildCreate", guild => {
-	init(guild);
+client.on("guildCreate", (guild) => {
+  init(guild);
 });
 
 client.on("ready", () => {
-	client.user
-		.setPresence({
-			status: "online",
-			afk: false,
-			game: {
-				name: "Use " + botPrefix + "help for help!"
-			}
-		})
-		.then(console.log("Bot ready."));
+  client.user
+    .setPresence({
+      status: "online",
+      afk: false,
+      game: {
+        name: "Use " + botPrefix + "help for help!",
+      },
+    })
+    .then(console.log("Bot ready."));
 
-	for (let i = 0; i < client.guilds.array().length; i++) {
-		init(client.guilds.array()[i]);
-	}
+  for (let i = 0; i < client.guilds.array().length; i++) {
+    init(client.guilds.array()[i]);
+  }
 
-	schedule.scheduleJob("*/1 * * * * *", function() {
-		for (let i = 0; i < client.guilds.array().length; i++) {
-			checkStatus(client.guilds.array()[i]);
-		}
-	});
+  schedule.scheduleJob("*/1 * * * * *", function () {
+    for (let i = 0; i < client.guilds.array().length; i++) {
+      checkStatus(client.guilds.array()[i]);
+    }
+  });
 });
